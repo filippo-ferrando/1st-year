4253a2668954fb6428682ca3cf490a3f966c7a8f
filippo-ferrando <ferrandodfilippo@protonmail.com>
2023-03-08 09:26:58 +0100

    update es arch

---
+++ 2-Semestre/ArchElab/Esercizi/C/Codifica/es1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main(){
+    uint16_t counter = 0;   //char 0->127, -128->0, di nuovo
+                            //uint8_t 0->255, overflow a 0
+                            //uint16_t 0->512, no overflow
+
+    for(int i = 0; i <= 512; i += 1){
+        printf("value: %d\n", counter);
+        counter += 1;
+    }
+
+}
---
+++ 2-Semestre/ArchElab/Esercizi/C/Codifica/es2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+    float from = 1;
+    float to = 10;
+    float step = 0.1;
+
+    for(float index = from; index <= to; index += step){
+        printf("value: %f\n", index);
+    }
+}
+
+//l'errore introdotto è dovuto alla codifica in virgola mobile, che non è esatta e introduce un errore già visibile alla seconda cifra decimale dopo relativaente poche iterazioni
