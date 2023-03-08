b0cf34ba03811b25a5fce2e9c5089c48de6ee082
filippo-ferrando <ferrandodfilippo@protonmail.com>
2023-03-08 19:01:50 +0100

    update

--- 2-Semestre/ArchElab/Esercizi/C/Codifica/es1.c
+++ 2-Semestre/ArchElab/Esercizi/C/Codifica/es1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void main(){
+int main(){
     uint16_t counter = 0;   //char 0->127, -128->0, di nuovo
                             //uint8_t 0->255, overflow a 0
                             //uint16_t 0->512, no overflow
@@ -10,5 +10,5 @@ void main(){
         printf("value: %d\n", counter);
         counter += 1;
     }
-
+    return 0;
 }
